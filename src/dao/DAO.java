997ef02ebb54606c8c8794867159ac5eab6328d4
nagano03 <nagano03@nagano03-HP>
2014-05-21 10:15:18 +0900

    おｋ？

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -10,7 +10,7 @@ public abstract class DAO {
 	protected Connection con = null;
 	protected Statement stmt = null;
 		
-	protected void getConnection() throws SQLException, ClassNotFoundException {
+	protected void getConnection() {
 		String drv = "com.mysql.jdbc.Driver";
 		
 		String url = "jdbc:mysql://localhost:3306/ver_ctrl_db";
@@ -20,29 +20,55 @@ public abstract class DAO {
 		if(this.con != null) {
 			return;
 		}
-		
+		try{
 		Class.forName(drv);
+		
+		}catch(ClassNotFoundException e){
+			System.out.println("[conect]異常" + e);
+		}
+		
+		try{
 		con = DriverManager.getConnection(url, user, pass);
+		return;
+		
+		}catch(SQLException e){
+			System.out.println("[conect]異常" + e);
+		}
 	}
 	
-	protected void createStmt() throws SQLException, ClassNotFoundException {
+	protected void createStmt()  {
 		if(this.stmt != null) {
 			return;
 		}
 		
 		getConnection();
 		
-		stmt = con.createStatement();
+		try{
+			stmt = con.createStatement();
+			
+		}catch(SQLException e){
+			System.out.println("[createStmt]異常" + e);
+		}
 	}
 	
-	protected void close() throws SQLException {
+	protected void close()  {
 		if(this.stmt != null) {
+			try{
 			this.stmt.close();
+			}catch(SQLException e){
+				System.out.println("[close]異常" + e);
+			}
 		}
+	
 		this.stmt = null;
 		
 		if(con != null) {
+			try{
 			this.con.close();
+			
+			}catch(SQLException e){
+				System.out.println("[close]異常" + e);
+			}
 		}
 		this.con = null;
 	}
