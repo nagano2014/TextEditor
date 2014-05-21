60e7a58a267cb323bbc36c212cb2a8ca07b1495f
nagano01 <nagano01@nagano01-HP>
2014-05-21 09:41:24 +0900

    search

--- src/util/Search.java
+++ src/util/Search.java
@@ -1,5 +1,26 @@
 package util;
 
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Search {
+public static JTextArea strSearch(JTextArea area,JTextField key){
+		
+		String textArea = area.getText();
+		String keyword = key.getText();
+
+		int start = textArea.indexOf(keyword);
+		int end = start + keyword.length();
+		
+		area.setSelectionStart(end+textArea.indexOf(keyword));
+		area.setSelectionEnd(start+keyword.length());
+
+		area.requestFocus(true);
+		return area;
+	}
+	
+	public boolean canReplace(JTextArea area, String key){
+		return area.getText().indexOf(key) != -1;
+	}
 
 }
