45a3369e92e36365423de74a919024157c14f0a1
nagano03 <nagano03@nagano03-HP>
2014-05-21 11:09:30 +0900

    おそらくおｋ

--- src/entity/FileEt.java
+++ src/entity/FileEt.java
@@ -1,5 +1,50 @@
 package entity;
 
+import java.util.Date;
+
 public class FileEt {
+	private int mFileId;
+	private String mFilePath;
+	private Date mFileMakeDay;
+	
+	public FileEt(){
+		
+	}
+	
+	public FileEt(int fileid, String filepath, Date filemd){
+		mFileId = fileid;
+		mFilePath = filepath;
+		mFileMakeDay = filemd;
+	}
+	
+	public int getFileid(){
+		return mFileId;
+	}
+	
+	public void setFileid(int fileid){
+		mFileId = fileid;
+	}
+	
+	public String getFilepath(){
+		return mFilePath;
+	}
+	
+	public void setFileid(String filepath){
+		mFilePath = filepath;
+	}
+	
+	public Date getFilemd(){
+		return mFileMakeDay;
+	}
+	
+	public void setFileid(Date filemd){
+		mFileMakeDay = filemd;
+	}
+	
+	@Override
+	public String toString(){
+		String buffer = "[FileEt: fileid:"+mFileId +" filepath:"+ mFilePath +" filemd:" + mFileMakeDay +"]";
+		return buffer;
+	}
 	
 }
--- src/entity/TextEt.java
+++ src/entity/TextEt.java
@@ -1,5 +1,49 @@
 package entity;
 
+
 public class TextEt {
+	private int mFileId;
+	private int mLineNumber;
+	private String mLineContents;
+	
+	public TextEt(){
+		
+	}
+	
+	public TextEt(int fileid, int linenum, String linecon){
+		mFileId = fileid;
+		mLineNumber = linenum;
+		mLineContents = linecon;
+	}
+	
+	public int getFileid(){
+		return mFileId;
+	}
+	
+	public void setFileid(int fileid){
+		mFileId = fileid;
+	}
+	
+	public int getLinenumber(){
+		return mLineNumber;
+	}
+	
+	public void setLinenumber(int linenum){
+		mLineNumber = linenum;
+	}
+	
+	public String getLinecontents(){
+		return mLineContents;
+	}
+	
+	public void setFileid(String linecon){
+		mLineContents = linecon;
+	}
+	
+	@Override
+	public String toString(){
+		String buffer = "[FileEt: fileid:"+mFileId +" linenum:"+ mLineNumber +" linecon:" + mLineContents +"]";
+		return buffer;
+	}
 
 }
