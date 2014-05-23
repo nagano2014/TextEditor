2e6ed7480da9c49f57f46c783fbb6ef698338630
nagano02 <nagano02@nagano02-HP>
2014-05-23 09:52:40 +0900

    あ

--- src/util/Replacement.java
+++ src/util/Replacement.java
@@ -11,27 +11,28 @@ public class Replacement{
 	/**
 	 * 単語の置換
 	 * @param area 本文
-	 * @param beforeWord 置換前の単語
-	 * @param afterWord 置換後の単語
-	 * @return 置換後の本文
+	 * @param oldWord 置換前の単語
+	 * @param newWord 置換後の単語
+	 * @return 置換が完了すればtrue
 	 */
-	public JTextArea strReplace(JTextArea area, String beforeWord, String afterWord){
-		while(area.getText().indexOf(beforeWord) != -1 && !beforeWord.equals("")){
-			int start = area.getText().indexOf(beforeWord);
-			int end = start + beforeWord.length();
-			area.replaceRange(afterWord, start, end);
+	public static boolean strReplace(JTextArea area, String oldWord, String newWord){
+		boolean result = canReplace(area, oldWord);
+		if(result){
+			String str = area.getText();
+			int index = str.lastIndexOf(oldWord);
+			area.setText(str.replaceAll(oldWord, newWord));
+			//テキスト内で最後に置換された部分を選択状態にする
+			area.select(index, index + newWord.length());
 		}
-		//テキスト内で最後に置換された部分を選択状態にする
-		area.select(area.getText().lastIndexOf(afterWord), area.getText().lastIndexOf(afterWord) + afterWord.length());
-		return area;
+		return result;
 	}
 	/**
 	 * 置換可能ならtrue、不可能ならfalseを返す
 	 * @param area 本文
-	 * @param beforeWord 置換前の単語
+	 * @param oldWord 置換前の単語
 	 * @return 置換可能ならtrue
 	 */
-	public boolean canReplace(JTextArea area, String beforeWord){
-		return area.getText().indexOf(beforeWord) != -1 && !area.getText().equals("") && !beforeWord.equals("");
+	private static boolean canReplace(JTextArea area, String oldWord){
+		return area.getText().indexOf(oldWord) != -1 && !area.getText().equals("") && !oldWord.equals("");
 	}
 }
